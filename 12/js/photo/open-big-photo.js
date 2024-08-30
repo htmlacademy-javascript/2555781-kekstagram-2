c88f08f2886acbc626757903401eebf4c1be23dc
Keks <keks@htmlacademy.ru>
2024-08-30 17:01:01 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/photo/open-big-photo.js
@@ -0,0 +1,25 @@
+import {descriptionPhoto} from './create-description-photo';
+import {createBigPhoto, onBigPhotoEscKeydownClose, closeBigPhoto} from './create-big-photo';
+
+const bigPhotoBlock = document.querySelector('.big-picture');
+const pictureCollection = document.querySelector('.pictures');
+const body = document.querySelector('body');
+const buttonClose = bigPhotoBlock.querySelector('.cancel');
+
+const openBigPhoto = function(evt) {
+  const pictureElement = evt.target.closest('.picture');
+  if (pictureElement) {
+    const photoId = pictureElement.querySelector('.picture__img').dataset.id;
+    const objectPhoto = descriptionPhoto.find((item) => photoId === String(item.id));
+    bigPhotoBlock.classList.remove('hidden');
+    createBigPhoto(objectPhoto);
+    body.classList.add('modal-open');
+    buttonClose.addEventListener('click', closeBigPhoto);
+    document.addEventListener('keydown', onBigPhotoEscKeydownClose);
+  }
+};
+
+pictureCollection.addEventListener('click', openBigPhoto);
+
+
+export {openBigPhoto};
