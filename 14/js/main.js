a474deb762ad12e4fb9ffe24e6b4dc7b675caf43
Keks <keks@htmlacademy.ru>
2024-09-02 13:18:24 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/main.js
@@ -0,0 +1,6 @@
+import './photo/add-user-photo-main-page.js';
+import './photo/open-big-photo.js';
+import './validation/create-upload-photo.js';
+import './validation/validation.js';
+import './photo/edite-photo/edite-photo.js';
+
