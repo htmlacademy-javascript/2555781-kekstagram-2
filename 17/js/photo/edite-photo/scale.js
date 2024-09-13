f5feba3eced81898f2e5951d59110804b98fd37b
Keks <keks@htmlacademy.ru>
2024-09-13 15:43:07 +0300

    :heavy_check_mark: Сборка #17

---
+++ 17/js/photo/edite-photo/scale.js
@@ -0,0 +1,38 @@
+const scaleContainerElement = document.querySelector('.img-upload__overlay');
+const photoElement = document.querySelector('.img-upload__preview img');
+const scaleValueElement = scaleContainerElement.querySelector('.scale__control--value');
+const scaleSmallerButtonElement = scaleContainerElement.querySelector('.scale__control--smaller');
+const scaleBiggerButtonElement = scaleContainerElement.querySelector('.scale__control--bigger');
+
+const SCALE_SETTINGS = {
+  STEP: 25,
+  MIN: 25,
+  MAX: 100,
+  DIVIDER: 100,
+};
+
+let currentScale = SCALE_SETTINGS.MAX;
+
+const updatePhotoScale = (scaleValue) => {
+  photoElement.style.transform = `scale(${scaleValue / SCALE_SETTINGS.DIVIDER})`;
+  scaleValueElement.value = `${scaleValue}%`;
+};
+
+const onScaleBiggerButtonClick = () => {
+  if (currentScale < SCALE_SETTINGS.MAX) {
+    currentScale += SCALE_SETTINGS.STEP;
+    updatePhotoScale(currentScale);
+  }
+};
+
+const onScaleSmallerButtonClick = () => {
+  if (currentScale > SCALE_SETTINGS.MIN) {
+    currentScale -= SCALE_SETTINGS.STEP;
+    updatePhotoScale(currentScale);
+  }
+};
+
+scaleBiggerButtonElement.addEventListener('click', onScaleBiggerButtonClick);
+scaleSmallerButtonElement.addEventListener('click', onScaleSmallerButtonClick);
+
+export {updatePhotoScale};
---
+++ 17/js/util.js
@@ -0,0 +1,18 @@
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
+
+export {isEscapeKey, debounce, shuffle};
